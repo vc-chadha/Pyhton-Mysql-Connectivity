3c7f89f7209a89289959e3634dae8058323388a2
vc-chadha <vasuchadha007@gmail.com>
2024-06-01 22:35:25 +0530

    Update TESTCODE2.py

--- TESTCODE2.py
+++ TESTCODE2.py
@@ -89,23 +89,6 @@ create_table_and_insert_data()
 
 
 
-##import pandas as pd
-##import mysql.connector as con
-##
-##def exporting():
-##    db_name = input("Enter the database name: ")
-##
-##    db = con.connect(host="localhost", user="root", password="12345", database=db_name)
-##
-##    query = "SELECT * FROM information"
-##    df = pd.read_sql(query, con=db)
-##
-##    df.to_excel("mysql_data.xlsx", index=False)
-##    print("MySQL data successfully exported to Excel file.")
-##
-##    db.close()
-##
-##exporting()
 
 
 
